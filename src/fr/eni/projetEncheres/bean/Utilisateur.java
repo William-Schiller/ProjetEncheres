f4a79d771776e5c1c89ba84ee42f19277fea8551
William SCHILLER <william.schiller.07@gmail.com>
2021-02-09 09:05:13 +0100

    mise en place premier fichier java

---
+++ src/fr/eni/projetEncheres/bean/Utilisateur.java
@@ -0,0 +1,9 @@
+package fr.eni.projetEncheres.bean;
+
+import java.io.Serializable;
+
+public class Utilisateur implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/fr/eni/projetEncheres/bll/UtilisateurManager.java
@@ -0,0 +1,5 @@
+package fr.eni.projetEncheres.bll;
+
+public class UtilisateurManager {
+
+}
