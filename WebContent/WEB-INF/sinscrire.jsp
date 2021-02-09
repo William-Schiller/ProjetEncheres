7432e908d5df0569c49e58dd4f558cd74936f3b2
Rose Dorleans <rosedorleans1@gmail.com>
2021-02-09 12:07:23 +0100

    ajout accueil.jsp + accueilConnecte.jsp + sinscrire.jsp + ServletSinscrire

---
+++ WebContent/WEB-INF/sinscrire.jsp
@@ -0,0 +1,64 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<title>S'inscrire</title>
+</head>
+<body>
+	<h1>S'INSCRIRE</h1>
+	<div class="container-fluid">
+		<div class="row">
+			<h2>Mon profil</h2>
+			<div>
+				<form action="<%=request.getContextPath() %>/inscription" method="post">
+					<div class="col-md-6">
+						<div>
+							Pseudo : <input type="text" name="spseudo" value="<%=request.getParameter("spseudo") %>" > 
+						</div>
+						<div>
+							Prénom : <input type="text" name="sprenom" value="<%=request.getParameter("sprenom") %>" > 
+						</div>
+						<div>
+							Téléphone : <input type="tel" name="stelephone" value="<%=request.getParameter("stelephone") %>" > 
+						</div>
+						<div>
+							Code postal : <input type="number" name="spostal" value="<%=request.getParameter("spostal") %>" > 
+						</div>
+						<div>
+							Mot de passe : <input type="text" name="smdp" value="<%=request.getParameter("smdp") %>" > 
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div>
+							Nom : <input type="text" name="snom" value="<%=request.getParameter("snom") %>" > 
+						</div>
+						<div>
+							Email : <input type="email" name="semail" value="<%=request.getParameter("semail") %>" > 
+						</div>
+						<div>
+							Rue : <input type="text" name="srue" value="<%=request.getParameter("srue") %>" > 
+						</div>
+						<div>
+							Ville : <input type="text" name="sville" value="<%=request.getParameter("sville") %>" > 
+						</div>
+						<div>
+							Confirmation : <input type="text" name="sconfirmation" value="<%=request.getParameter("sconfirmation") %>" > 
+						</div>
+					</div>
+					<br>
+					<div>
+						<input type="submit" value="Creer">
+						<a href="<%=request.getContextPath()%>">
+							<input type="button" value="Annuler">
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ src/fr/eni/projetEncheres/servlet/ServletSinscrire.java
@@ -0,0 +1,69 @@
+package fr.eni.projetEncheres.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.projetEncheres.bean.Utilisateur;
+
+
+
+@WebServlet("/inscription")
+public class ServletSinscrire extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//private UtilisateurDAO UtilisateurDAO = new UtilisateurDAO();
+
+    public ServletSinscrire() {
+        super();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/sinscrire.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String pseudo = request.getParameter("pseudo");
+		String prenom = request.getParameter("prenom");
+		String telephone = request.getParameter("telephone");
+		int postal = Integer.parseInt(request.getParameter("postal"));
+		String mdp = request.getParameter("mdp");
+		String nom = request.getParameter("nom");
+		String email = request.getParameter("email");
+		String rue = request.getParameter("rue");
+		String ville = request.getParameter("ville");
+		
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setPseudo(pseudo);
+		utilisateur.setPrenom(prenom);
+		utilisateur.setTelephone(telephone);
+		utilisateur.setCode_postal(postal);
+		utilisateur.setMot_de_passe(mdp);
+		utilisateur.setNom(nom);
+		utilisateur.setEmail(email);
+		utilisateur.setRue(rue);
+		utilisateur.setVille(ville);
+
+
+		try {
+			//UtilisateurManager.inscriptionUtilisateur(utilisateur);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/accueilConnecte.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
