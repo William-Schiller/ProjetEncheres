facee3f6e74c188a4dcb2ea237e5e8f8d7d5ffe1
William SCHILLER <william.schiller.07@gmail.com>
2021-02-09 12:02:29 +0100

    MAJ utilisateur Manager

--- src/fr/eni/projetEncheres/bll/UtilisateurManager.java
+++ src/fr/eni/projetEncheres/bll/UtilisateurManager.java
@@ -1,5 +1,9 @@
 package fr.eni.projetEncheres.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.projetEncheres.bean.Utilisateur;
 import fr.eni.projetEncheres.dal.DAOFactory;
 import fr.eni.projetEncheres.dal.UtilisateurDAO;
 
@@ -7,6 +11,8 @@ public class UtilisateurManager {
 	
 	private static UtilisateurManager utilisateurManager;
 	private UtilisateurDAO utilisateurDAO;
+	
+	private static List<String> listError;
 	
 	private UtilisateurManager() {
 		this.utilisateurDAO = DAOFactory.getUtilisateurDAO();
@@ -19,5 +25,49 @@ public class UtilisateurManager {
 		return utilisateurManager;
 	}
 	
+	/**
+	 * @author : sw
+	 * @throws BLLException 
+	 */
+	public Utilisateur connexionUser(String pseudo, String mot_de_pass) throws BLLException {
+		
+		listError = new ArrayList<String>();
+		Utilisateur user = null;
+		
+		checkPseudo(pseudo, listError);
+		checkPassword(mot_de_pass, listError);
+		
+		if(listError != null) {
+			throw new BLLException("Echec connexionUser : vérification pseudo et mot de passe");
+		}
+		
+		user = utilisateurManager.connexionUser(pseudo, mot_de_pass);
+			
+		return user;
+	}
+	
+	public List<String> getListError() {
+		return listError;
+	}
+	
+	/**
+	 * @author : sw
+	 */
+	public void checkPseudo(String pseudo, List<String> listError){
+		if(pseudo.length() > 30) {
+			listError.add("Le pseudonyme ne doit pas dépasser 30 caractères");
+		}
+			
+	}
+	
+	/**
+	 * @author : sw
+	 */
+	public void checkPassword(String mot_de_pass, List<String> listError){
+		if(mot_de_pass.length() > 30) {
+			listError.add("Le mot de passe ne doit pas dépasser 30 caractères");
+		}
+			
+	}
 
 }
