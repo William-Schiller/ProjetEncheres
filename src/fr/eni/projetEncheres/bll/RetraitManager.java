2f4a0da842dfdc2371ef24a8281765831c0068d5
Aurélien <Aurlien@DESKTOP-22VB3N2.home>
2021-02-15 16:58:36 +0100

    retraitManager

--- src/fr/eni/projetEncheres/bll/RetraitManager.java
+++ src/fr/eni/projetEncheres/bll/RetraitManager.java
@@ -6,64 +6,85 @@ import java.util.List;
 
 
 import fr.eni.projetEncheres.bean.Retrait;
+import fr.eni.projetEncheres.bean.Utilisateur;
 import fr.eni.projetEncheres.dal.DALException;
 import fr.eni.projetEncheres.dal.DAO;
 import fr.eni.projetEncheres.dal.DAOFactory;
 
 public class RetraitManager {
 
-	
+
 	private static RetraitManager retraitManager;
-	
+
 	private DAO <Retrait> retraitDAO;
-	
-	
+
+
 	private RetraitManager() {
 		this.retraitDAO =  DAOFactory.getRetraitDAO();
-		
+
 	}
-	
-	
+
+
 	public static RetraitManager getInstance() {
 		if(retraitManager == null) {
 			retraitManager = new RetraitManager();
 		}
 		return retraitManager;
 	}
-	
-	
-	public void insertRetrait(Retrait t) throws BLLException {
-		Retrait insertretrait = null;
-		
-		
-		try {
-			
-		  retraitDAO.insert(t);
-			
-		
-		} catch (DALException e) {
-			throw new BLLException("Echec method insert()");
-		
-		
-		}
-		
-	}	
-				
-			
 
-	
-public void deleteRetrait(Retrait t) throws BLLException {
-		
+
+	public void insertRetrait(Retrait t) throws BLLException{
+		boolean verifRetraitExist = false;
+		List<Retrait> listeRetrait = new ArrayList<>();
+
+
+		//Recuperer la liste complete des retrait
+
 		try {
-			
+			listeRetrait = retraitDAO.selectAll();
+		} catch (DALException e1) {
+			throw new BLLException("Echec method insertRetrait()");
+
+		}
+
+		//tester tous le contenue de la liste pour vérifier si les retrait existe
+		for (Retrait retrait : listeRetrait) {
+			if(t.getVille().equals(retrait.getVille()) && t.getCode_postale() == retrait.getCode_postale() && t.getRue().equals(retrait.getRue())) {
+				//Vérifier si les parametre de t (test le retrait passé en parametre de la methode) sont egaux aux parametre de retrait ( retrait est un element retrait de la liste complete des retrait)
+				//si le retrait existe recuperer le retrait dans une variable
+				t=retrait;
+				verifRetraitExist = true;
+				break;
+			}
+
+		}
+
+		if (!verifRetraitExist) {
+			try {
+				retraitDAO.insert(t);
+			} catch (DALException e) {
+				throw new BLLException("Echec method insertRetrait()");
+			}
+		}
+
+
+	}	
+
+
+
+
+	public void deleteRetrait(Retrait t) throws BLLException {
+
+		try {
+
 			retraitDAO.delete(t.getNo_retrait());
-		
+
 		} catch (DALException e) {
-			
+
 			throw new BLLException("Echec deleteRetrait");
 		}
-		
+
 	}
-	
-	
+
+
 }
