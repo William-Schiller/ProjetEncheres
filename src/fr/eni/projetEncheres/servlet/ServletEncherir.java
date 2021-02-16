2862ee7fea4dad5b3a3384312f7b748a67c4ba11
Rose Dorleans <rosedorleans1@gmail.com>
2021-02-16 14:55:19 +0100

    maj encherir

--- src/fr/eni/projetEncheres/servlet/ServletEncherir.java
+++ src/fr/eni/projetEncheres/servlet/ServletEncherir.java
@@ -25,7 +25,7 @@ public class ServletEncherir extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/detailsVente.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/detailsEnchere.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
